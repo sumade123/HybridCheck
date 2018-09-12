27111a0508a966bfd720ca8d57500d010e355164
sumade123 <41547641+sumade123@users.noreply.github.com>
2018-09-12 21:18:44 +0100

    Update TripletReference.R

--- R/TripletReference.R
+++ R/TripletReference.R
@@ -249,7 +249,7 @@ bars and the NaNs will be dealt with my filling them in black.\n\nTo get rid of 
                                  legend[transparent] <- NA
                                }
                                legendgrob <- grid::rasterGrob(image=legend)
-                               bars <- arrangeGrob(bars, legendgrob, widths = c(1, 0.15), ncol = 2,left=textGrob("\n\n\n\n"))
+                               bars <- arrangeGrob(bars, legendgrob, widths = c(1, 0.15), ncol = 2,left=("\n\n\n\n"))
                              }
                              return(bars)
                            }
