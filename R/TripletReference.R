d0791b99af589ae67a5ef29fa20bf07974088a6c
sumade123 <41547641+sumade123@users.noreply.github.com>
2020-04-27 14:59:44 +0100

    Update TripletReference.R

--- R/TripletReference.R
+++ R/TripletReference.R
@@ -187,7 +187,7 @@ Triplet <- setRefClass("Triplet",
                              plot <- ggplot(plotting.frame, aes(x=basepos/1000, y=yvalues)) + geom_line(aes(colour=factor(factors)), show_guide=plottingSettings$Legends, size=0.8) + scale_x_continuous(breaks = scales::pretty_breaks(n = 6), expand = c(0.0125,0)) +
                                ylim(0,100) + 
                                xlab("kb") +
-                               ylab("Sequence similarity (%)") + scale_colour_manual(name = "", labels=c(gsub("Rpi-","",combo[1]), gsub("Rpi-","",combo[2]), gsub("Rpi-","",combo[3])),values=c("yellow","purple","cyan")) + theme(legend.background = element_rect(fill = "white")) + theme(legend.key = element_rect(fill = "transparent")) + theme(panel.grid.major = element_line(size = 1)) #+ opts(panel.grid.major = none, panel.grid.minor = none)
+                               ylab("Sequence similarity (%)") + scale_colour_manual(name = "", labels=c(gsub("Rpi-","",combo[1]), gsub("Rpi-","",combo[2]), gsub("Rpi-","",combo[3])),values=c("yellow","purple","cyan")) + theme(legend.background = element_rect(fill = "white")) + theme(legend.key = element_rect(fill = "transparent")) + theme(panel.grid.major = element_line(size = 1)) + theme(panel.grid.minor = element_line(size = 1))#+ opts(panel.grid.major = none, panel.grid.minor = none)
                              plot <- applyPlottingParams(plot, plottingSettings) + theme(axis.title.y = element_text(size = 15,margin = unit(c(0, 4, 0, 0), "mm")),axis.title.x = element_text(size = 15,margin = unit(c(3.5, 0, 0, 0), "mm")),axis.text.y = element_text(size = 12),axis.text.x = element_text(size = 12)) # title = paste("Sequence Similarity Between Sequences for Triplet ", ContigNames[1], ":", ContigNames[2], ":", ContigNames[3], sep="")
                                                             recg <- rectGrob(gp=gpar(fill="white",col="white"))
                              recg <- rectGrob(gp=gpar(fill="white",col="white"))
@@ -245,7 +245,7 @@ bars and the NaNs will be dealt with my filling them in black.\n\nTo get rid of 
                              bars <- bars + theme(axis.title.y=element_blank(),axis.text.x=element_blank(),axis.title.x=element_blank(),axis.ticks.x=element_blank()) + theme(plot.margin=unit(c(5.5, 5.5, 5.5, 10), "points")) + theme(panel.grid.major = element_blank())#+ theme(legend.box.margin=margin(10,10,10,10))#bars <- bars + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line.x = element_line(colour = "black")) + theme(axis.text.y=element_blank(),axis.title.y=element_blank())##axis.ticks.y=element_blank(),axis.title.y=element_blank(),panel.background=element_blank(),panel.grid.major=element_blank(),panel.grid.minor=element_blank(),plot.background=element_blank()) 
                              
                              if(plottingSettings$Legends == T){
-                               legend <- readPNG(system.file("extdata/rgblegend.png", package="HybridCheck"), TRUE)
+                               legend <- readTIFF(system.file("extdata/rgblegend.tiff", package="HybridCheck"), TRUE)
                                #if (names(dev.cur()) == "windows"){
                                 # # windows device doesn’t support semi-transparency so we’ll need
                                  ## to flatten the image
