ef4cdefe20572ce979566ebc674b6575de377d4e
sumade123 <41547641+sumade123@users.noreply.github.com>
2020-04-27 20:43:57 +0100

    Update TripletReference.R

--- R/TripletReference.R
+++ R/TripletReference.R
@@ -254,8 +254,8 @@ bars and the NaNs will be dealt with my filling them in black.\n\nTo get rid of 
                                #}
                                legendgrob <- grid::rasterGrob(image=legend)
                                recg <- rectGrob(gp=gpar(fill="white",col="white"))
-                               legendgrob2 <- arrangeGrob(recg,legendgrob,ncol=1,nrow=2,heights=c(0.6,plottingSettings$RgbSize))#unit(c(plottingSettings$tl,plottingSettings$tm,plottingSettings$RgbSize),c("in","in"))
-                               bars <- arrangeGrob(recg, bars, legendgrob2, widths = c(plottingSettings$tl,plottingSettings$tm,plottingSettings$RgbSize), ncol = 3)#, heights = c(15,15,15))                       
+                               legendgrob2 <- arrangeGrob(recg,legendgrob,ncol=1,nrow=2,heights=c(0.1,plottingSettings$RgbSize))#unit(c(plottingSettings$tl,plottingSettings$tm,plottingSettings$RgbSize),c("in","in"))
+                               bars <- arrangeGrob(recg, bars, legendgrob, widths = c(plottingSettings$tl,plottingSettings$tm,plottingSettings$RgbSize), ncol = 3)#, heights = c(15,15,15))                       
                              }
                              return(bars)
                            }
