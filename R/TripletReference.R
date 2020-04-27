65f0c5877420f4689cb2952601c8aba935addac1
sumade123 <41547641+sumade123@users.noreply.github.com>
2020-04-27 14:40:48 +0100

    Update TripletReference.R

--- R/TripletReference.R
+++ R/TripletReference.R
@@ -189,7 +189,10 @@ Triplet <- setRefClass("Triplet",
                                xlab("kb") +
                                ylab("Sequence similarity (%)") + scale_colour_manual(name = "", labels=c(gsub("Rpi-","",combo[1]), gsub("Rpi-","",combo[2]), gsub("Rpi-","",combo[3])),values=c("yellow","purple","cyan")) + theme(legend.background = element_rect(fill = "white")) + theme(legend.key = element_rect(fill = "transparent")) + theme(panel.grid.major = element_blank()) #+ opts(panel.grid.major = none, panel.grid.minor = none)
                              plot <- applyPlottingParams(plot, plottingSettings) + theme(axis.title.y = element_text(size = 15,margin = unit(c(0, 4, 0, 0), "mm")),axis.title.x = element_text(size = 15,margin = unit(c(3.5, 0, 0, 0), "mm")),axis.text.y = element_text(size = 12),axis.text.x = element_text(size = 12)) # title = paste("Sequence Similarity Between Sequences for Triplet ", ContigNames[1], ":", ContigNames[2], ":", ContigNames[3], sep="")
-                             return(plot)
+                                                            recg <- rectGrob(gp=gpar(fill="white",col="white"))
+                             recg <- rectGrob(gp=gpar(fill="white",col="white"))
+                             lines <- arrangeGrob(recg, lines, widths=c(0.07,0.9),ncol=2)                  
+                             return(lines)
                            },
                          
                          plotBars =
